a810460b2bff1026fbc2d31d7a1f5af1bc1cbdd1
yaswanthyaswanth <yaswanthyaswanth76@gmail.com>
2025-05-20 16:20:01 +0530

    Fix

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,75 +1,75 @@
 import React, { Suspense, useEffect, useState } from "react";
-     import { Canvas } from "@react-three/fiber";
-     import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
-     import CanvasLoader from "../Loader";
+import CanvasLoader from "../Loader";
 
-     const Computers = ({ isMobile }) => {
-       const computer = useGLTF("./desktop_pc/scene.gltf");
+const Computers = ({ isMobile }) => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
 
-       return (
-         <mesh>
-           <hemisphereLight intensity={0.15} groundColor='black' />
-           <spotLight
-             position={[-20, 50, 10]}
-             angle={0.12}
-             penumbra={1}
-             intensity={1}
-             castShadow
-             shadow-mapSize={1024}
-           />
-           <pointLight intensity={1} />
-           <primitive
-             object={computer.scene}
-             scale={isMobile ? 0.7 : 0.75}
-             position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-             rotation={[-0.01, -0.2, -0.1]}
-           />
-         </mesh>
-       );
-     };
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.50} groundColor='black' />
+      <spotLight
+        position={[-20, 80, -80]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <pointLight intensity={1} />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.6 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.6, -1.5]}
+        rotation={[-0.01, -0.8, -0.1]}
+      />
+    </mesh>
+  );
+};
 
-     const ComputersCanvas = () => {
-       const [isMobile, setIsMobile] = useState(false);
+const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
 
-       useEffect(() => {
-         const mediaQuery = window.matchMedia("(max-width: 500px)");
-         setIsMobile(mediaQuery.matches);
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    setIsMobile(mediaQuery.matches);
 
-         const handleMediaQueryChange = (event) => {
-           setIsMobile(event.matches);
-         };
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
 
-         mediaQuery.addEventListener("change", handleMediaQueryChange);
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
 
-         return () => {
-           mediaQuery.removeEventListener("change", handleMediaQueryChange);
-         };
-       }, []);
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
 
-       return (
-         <Canvas
-           frameloop='demand'
-           shadows
-           dpr={[1, 2]}
-           camera={{ position: [20, 3, 5], fov: 25 }}
-           gl={{ preserveDrawingBuffer: true }}
-           className="touch-pinch-zoom" // Add class for CSS
-         >
-           <Suspense fallback={<CanvasLoader />}>
-             <OrbitControls
-               enableZoom={true} // Enable zooming
-               enablePan={false} // Disable panning to focus on rotation and zoom
-               maxPolarAngle={Math.PI / 2}
-               minPolarAngle={Math.PI / 2}
-               minDistance={10} // Minimum zoom distance
-               maxDistance={50} // Maximum zoom distance
-             />
-             <Computers isMobile={isMobile} />
-           </Suspense>
-           <Preload all />
-         </Canvas>
-       );
-     };
+  return (
+    <Canvas
+      frameloop='demand'
+      shadows
+      dpr={[1, 2]}
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+      className="touch-pinch-zoom"
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          enablePan={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+          minDistance={10}
+          maxDistance={50}
+        />
+        <Computers isMobile={isMobile} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
 
-     export default ComputersCanvas;
+export default ComputersCanvas;
